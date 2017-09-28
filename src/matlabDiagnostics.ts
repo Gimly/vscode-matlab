e08a37f5e7f256a11b019373ea0a7479db90b8eb
Xavier Hahn <xavier.hahn@gmail.com>
2017-09-28 22:19:08 +0200

    Added encoding option for the linting
    * Added setting to change the encoding read from the linter
    * Used this setting in the child_process call to change the encoding on which it will be read.

--- src/matlabDiagnostics.ts
+++ src/matlabDiagnostics.ts
@@ -4,7 +4,7 @@ import vscode = require('vscode');
 import path = require('path');
 import cp = require('child_process');
 
-import {window} from 'vscode';
+import { window } from 'vscode';
 
 export interface ICheckResult {
 	file: string;
@@ -18,33 +18,45 @@ export function check(filename: string, lintOnSave = true, mlintPath = ""): Prom
 	var matlabLint = !lintOnSave ? Promise.resolve([]) : new Promise((resolve, reject) => {
 		var filename = window.activeTextEditor.document.fileName;
 
-		cp.execFile(mlintPath, [filename], (err, stdout, stderr) => {
-			try {
-				var errors = stderr.split('\n');
+		let matlabConfig = vscode.workspace.getConfiguration('matlab');
 
-				var ret: ICheckResult[] = [];
+		let fileEncoding = 'utf8';
 
-				errors.forEach(error => {
-					var regex = /L (\d+) \(C (\d+)-?(\d+)?\): (.*)/;
-					var match = regex.exec(error);
+		if (matlabConfig.has('linterEncoding') && matlabConfig['linterEncoding'] != null) {
+			fileEncoding = matlabConfig['linterEncoding'];
+		}
 
-					if (match != null) {
-						var [_, lineStr, startCol, endCol, msg] = match;
-						var line = +lineStr;
+		cp.execFile(
+			mlintPath,
+			[filename],
+			{ encoding: fileEncoding } as cp.ExecFileOptionsWithStringEncoding,
+			(err, stdout, stderr) => {
+				try {
+					var errors = stderr.split('\n');
 
-						if(endCol == null){
-							endCol = startCol;
+					var ret: ICheckResult[] = [];
+
+					errors.forEach(error => {
+						var regex = /L (\d+) \(C (\d+)-?(\d+)?\): (.*)/;
+						var match = regex.exec(error);
+
+						if (match != null) {
+							var [_, lineStr, startCol, endCol, msg] = match;
+							var line = +lineStr;
+
+							if (endCol == null) {
+								endCol = startCol;
+							}
+
+							ret.push({ file: filename, line, column: [+startCol, +endCol], msg, severity: "warning" });
 						}
+					});
 
-						ret.push({ file: filename, line, column: [+startCol, +endCol], msg, severity: "warning" });
-					}
-				});
-
-				resolve(ret);
-			} catch (error) {
-				reject(error);
-			}
-		});
+					resolve(ret);
+				} catch (error) {
+					reject(error);
+				}
+			});
 	});
 
 	return Promise.all([matlabLint]).then(resultSets => [].concat.apply([], resultSets));
