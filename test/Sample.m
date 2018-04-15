d79eb4b6a8078f35c70b2705b33bcb637d9de296
SheldonZhong <374662347@qq.com>
2018-04-15 17:54:04 +0800

    WIP: New MatLab Function Command
    Working in creating matlab function command, add a Hello World version
    of the command. Learning more about the mechanisms of command.
    
    WIP: properties attributes. Reading more documents about specifying
    variables are place-holder.

---
+++ test/Sample.m
@@ -0,0 +1,96 @@
+for  = 1:n
+    
+end
+
+for index = 1:n
+    
+end
+
+for haha = das(dad)
+    
+end
+
+
+
+classdef (ClassAttributes) ClassName
+
+end
+    
+disp(sprintf('format, '});
+
+for idx = drange(3)
+    
+end
+
+fprintf(fileID, formatSpec, A1)
+
+switch variable
+case 'string'
+        
+end
+
+methods (MethodAttributes)
+    function obj = methodName(obj,args)
+            
+    end
+end
+
+classdef (ClassAttributes) ClassName
+    
+end
+
+
+ classdef (ClassAttributes) ClassName
+    
+end
+
+function 
+end
+
+
+methods (MethodAttributes)
+    function obj = methodName(obj,args)
+        
+    end
+end
+
+methods (Hello)
+    function adsj = dajdfl(fdjalk,kjfla)
+        
+    end
+end
+
+properties (|one,two,three|)
+    
+end
+
+properties (Constant)
+    
+end
+
+function output = asd(input)
+%asd - Description
+%
+% Syntax: output = asd(input)
+%
+% Long description
+    
+end
+
+function [ output ] = Sample(input)
+%Sample - Description
+%
+% Syntax: output = Sample(input)
+%
+% Long description
+    
+end
+
+function [ output ] = This(Hello)
+%This - E
+%
+% Syntax: output = This(Hello)
+%
+% Long description
+    
+end
