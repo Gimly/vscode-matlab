a85398d68d4b932e9983b77720ecbc0fd4ec1c4c
SNDST00M: M.U.N.I.N <82655227+4086606@users.noreply.github.com>
2021-07-30 08:16:54 +0100

    Use two-space indent in source code

--- src/matlabDiagnostics.ts
+++ src/matlabDiagnostics.ts
@@ -9,71 +9,71 @@ import { ERROR_IDS } from './errorsId';
 import { window } from 'vscode';
 
 export interface ICheckResult {
-	file: string;
-	line: number;
-	column: [number, number];
-	msg: string;
-	severity: string
+  file: string;
+  line: number;
+  column: [number, number];
+  msg: string;
+  severity: string
 }
 
 export function check(document: vscode.TextDocument, lintOnSave = true, mlintPath = ""): Promise<ICheckResult[]> {
-	var matlabLint = !lintOnSave ? Promise.resolve([]) : new Promise((resolve, reject) => {
-		var filename = document.uri.fsPath;
+  var matlabLint = !lintOnSave ? Promise.resolve([]) : new Promise((resolve, reject) => {
+    var filename = document.uri.fsPath;
 
-		let matlabConfig = vscode.workspace.getConfiguration('matlab');
+    let matlabConfig = vscode.workspace.getConfiguration('matlab');
 
-		let args: string[] = ['-all', '-id'];
-		if (matlabConfig.has('linterConfig') && matlabConfig['linterConfig'] != null) {
-			args.push(`-config=${matlabConfig['linterConfig']}`);
-		}
+    let args: string[] = ['-all', '-id'];
+    if (matlabConfig.has('linterConfig') && matlabConfig['linterConfig'] != null) {
+      args.push(`-config=${matlabConfig['linterConfig']}`);
+    }
 
-		args.push(filename);
+    args.push(filename);
 
-		let fileEncoding = 'utf8';
-		if (matlabConfig.has('linterEncoding') && matlabConfig['linterEncoding'] != null) {
-			fileEncoding = matlabConfig['linterEncoding'];
-		}
+    let fileEncoding = 'utf8';
+    if (matlabConfig.has('linterEncoding') && matlabConfig['linterEncoding'] != null) {
+      fileEncoding = matlabConfig['linterEncoding'];
+    }
 
-		cp.execFile(
-			mlintPath,
-			args,
-			{ encoding: 'buffer' },
-			(err: Error, stdout, stderr) => {
-				try {
-					let errorsString = iconv.decode(stderr, fileEncoding);
+    cp.execFile(
+      mlintPath,
+      args,
+      { encoding: 'buffer' },
+      (err: Error, stdout, stderr) => {
+        try {
+          let errorsString = iconv.decode(stderr, fileEncoding);
 
-					var errors = errorsString.split('\n');
+          var errors = errorsString.split('\n');
 
-					var ret: ICheckResult[] = [];
+          var ret: ICheckResult[] = [];
 
-					errors.forEach(error => {
-						var regex = /L (\d+) \(C (\d+)-?(\d+)?\): (\S+): (.*)/;
-						var match = regex.exec(error);
+          errors.forEach(error => {
+            var regex = /L (\d+) \(C (\d+)-?(\d+)?\): (\S+): (.*)/;
+            var match = regex.exec(error);
 
-						if (match != null) {
-							var [_, lineStr, startCol, endCol, idErrorWarn, msg] = match;
-							var line = +lineStr;
+            if (match != null) {
+              var [_, lineStr, startCol, endCol, idErrorWarn, msg] = match;
+              var line = +lineStr;
 
-							if (endCol == null) {
-								endCol = startCol;
-							}
+              if (endCol == null) {
+                endCol = startCol;
+              }
 
-							if (ERROR_IDS.includes(idErrorWarn)) {
-								ret.push({ file: filename, line, column: [+startCol, +endCol], msg, severity: "error" });
-							}
-							else {
-								ret.push({ file: filename, line, column: [+startCol, +endCol], msg, severity: "warning" });
-							}
-						}
-					});
+              if (ERROR_IDS.includes(idErrorWarn)) {
+                ret.push({ file: filename, line, column: [+startCol, +endCol], msg, severity: "error" });
+              }
+              else {
+                ret.push({ file: filename, line, column: [+startCol, +endCol], msg, severity: "warning" });
+              }
+            }
+          });
 
-					resolve(ret);
-				} catch (error) {
-					console.error(error);
-					reject(error);
-				}
-			});
-	});
+          resolve(ret);
+        } catch (error) {
+          console.error(error);
+          reject(error);
+        }
+      });
+  });
 
-	return Promise.all([matlabLint]).then(resultSets => [].concat.apply([], resultSets));
+  return Promise.all([matlabLint]).then(resultSets => [].concat.apply([], resultSets));
 }
--- src/matlabMain.ts
+++ src/matlabMain.ts
@@ -17,99 +17,99 @@ let diagnosticCollection: vscode.DiagnosticCollection;
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	console.log("Activating extension Matlab");
+  console.log("Activating extension Matlab");
 
-	context.subscriptions.push(
-		vscode.languages.registerDocumentSymbolProvider(
-			{ language: 'matlab', scheme: 'file' }, new MatlabDocumentSymbolProvider()
-		)
-	);
+  context.subscriptions.push(
+    vscode.languages.registerDocumentSymbolProvider(
+      { language: 'matlab', scheme: 'file' }, new MatlabDocumentSymbolProvider()
+    )
+  );
 
-	var matlabConfig = workspace.getConfiguration('matlab');
+  var matlabConfig = workspace.getConfiguration('matlab');
 
-	if (!matlabConfig['lintOnSave']) {
-		return;
-	}
+  if (!matlabConfig['lintOnSave']) {
+    return;
+  }
 
-	if (!matlabConfig.has('mlintpath') || matlabConfig['mlintpath'] == null) {
-		window.showErrorMessage("Could not find path to the mlint executable in the configuration file.")
-		return;
-	}
+  if (!matlabConfig.has('mlintpath') || matlabConfig['mlintpath'] == null) {
+    window.showErrorMessage("Could not find path to the mlint executable in the configuration file.")
+    return;
+  }
 
-	var mlintPath = matlabConfig['mlintpath'];
+  var mlintPath = matlabConfig['mlintpath'];
 
-	if (!fs.existsSync(mlintPath)) {
-		window.showErrorMessage("Cannot find mlint at the given path, please check your configuration file.")
-		return;
-	}
+  if (!fs.existsSync(mlintPath)) {
+    window.showErrorMessage("Cannot find mlint at the given path, please check your configuration file.")
+    return;
+  }
 
-	diagnosticCollection = vscode.languages.createDiagnosticCollection("matlab");
-	context.subscriptions.push(diagnosticCollection);
+  diagnosticCollection = vscode.languages.createDiagnosticCollection("matlab");
+  context.subscriptions.push(diagnosticCollection);
 
-	context.subscriptions.push(workspace.onDidSaveTextDocument(document => { lintDocument(document, mlintPath) }));
-	context.subscriptions.push(workspace.onDidOpenTextDocument(document => { lintDocument(document, mlintPath) }));
+  context.subscriptions.push(workspace.onDidSaveTextDocument(document => { lintDocument(document, mlintPath) }));
+  context.subscriptions.push(workspace.onDidOpenTextDocument(document => { lintDocument(document, mlintPath) }));
 
-	// Run mlint on any open documents since our onDidOpenTextDocument callback won't be hit for those
-	workspace.textDocuments.forEach(document => lintDocument(document, mlintPath));
+  // Run mlint on any open documents since our onDidOpenTextDocument callback won't be hit for those
+  workspace.textDocuments.forEach(document => lintDocument(document, mlintPath));
 
-	context.subscriptions.push(vscode.languages.registerDefinitionProvider(
-		['matlab'],
-		new PeekFileDefinitionProvider()
-	));
+  context.subscriptions.push(vscode.languages.registerDefinitionProvider(
+    ['matlab'],
+    new PeekFileDefinitionProvider()
+  ));
 
 }
 
 function lintDocument(document: vscode.TextDocument, mlintPath: string) {
 
-	function mapSeverityToVSCodeSeverity(sev: string) {
-		switch (sev) {
-			case "error": return vscode.DiagnosticSeverity.Error;
-			case "warning": return vscode.DiagnosticSeverity.Warning;
-			default: return vscode.DiagnosticSeverity.Error;
-		}
-	}
+  function mapSeverityToVSCodeSeverity(sev: string) {
+    switch (sev) {
+      case "error": return vscode.DiagnosticSeverity.Error;
+      case "warning": return vscode.DiagnosticSeverity.Warning;
+      default: return vscode.DiagnosticSeverity.Error;
+    }
+  }
 
-	if (document.languageId != "matlab" || document.uri.scheme != "file") {
-		return;
-	}
+  if (document.languageId != "matlab" || document.uri.scheme != "file") {
+    return;
+  }
 
-	let matlabConfig = vscode.workspace.getConfiguration('matlab');
+  let matlabConfig = vscode.workspace.getConfiguration('matlab');
 
-	check(document, matlabConfig['lintOnSave'], mlintPath).then(errors => {
-		diagnosticCollection.delete(document.uri);
+  check(document, matlabConfig['lintOnSave'], mlintPath).then(errors => {
+    diagnosticCollection.delete(document.uri);
 
-		let diagnosticMap: Map<vscode.Uri, vscode.Diagnostic[]> = new Map();;
+    let diagnosticMap: Map<vscode.Uri, vscode.Diagnostic[]> = new Map();;
 
-		errors.forEach(error => {
-			let targetUri = vscode.Uri.file(error.file);
+    errors.forEach(error => {
+      let targetUri = vscode.Uri.file(error.file);
 
-			var line = error.line - 1;
-			if (line < 0) line = 0;
+      var line = error.line - 1;
+      if (line < 0) line = 0;
 
-			var startColumn = error.column[0] - 1;
-			if (startColumn < 0) startColumn = 0;
+      var startColumn = error.column[0] - 1;
+      if (startColumn < 0) startColumn = 0;
 
-			var endColumn = error.column[1];
+      var endColumn = error.column[1];
 
-			let range = new vscode.Range(line, startColumn, line, endColumn);
-			let diagnostic = new vscode.Diagnostic(range, error.msg, mapSeverityToVSCodeSeverity(error.severity));
+      let range = new vscode.Range(line, startColumn, line, endColumn);
+      let diagnostic = new vscode.Diagnostic(range, error.msg, mapSeverityToVSCodeSeverity(error.severity));
 
-			let diagnostics = diagnosticMap.get(targetUri);
-			if (!diagnostics) {
-				diagnostics = [];
-			}
+      let diagnostics = diagnosticMap.get(targetUri);
+      if (!diagnostics) {
+        diagnostics = [];
+      }
 
-			diagnostics.push(diagnostic);
-			diagnosticMap.set(targetUri, diagnostics);
-		});
+      diagnostics.push(diagnostic);
+      diagnosticMap.set(targetUri, diagnostics);
+    });
 
-		let entries: [vscode.Uri, vscode.Diagnostic[]][] = [];
-		diagnosticMap.forEach((diags, uri) => {
-			entries.push([uri, diags]);
-		});
+    let entries: [vscode.Uri, vscode.Diagnostic[]][] = [];
+    diagnosticMap.forEach((diags, uri) => {
+      entries.push([uri, diags]);
+    });
 
-		diagnosticCollection.set(entries);
-	}).catch(err => {
-		vscode.window.showErrorMessage(err);
-	});
+    diagnosticCollection.set(entries);
+  }).catch(err => {
+    vscode.window.showErrorMessage(err);
+  });
 }
